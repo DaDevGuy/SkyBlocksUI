b63bd50436aff8c80e97fe88ef267411980e51e1
Ehan <contact@dadevguy.ml>
2022-03-15 16:45:11 +0500

    Fixed All Problems

--- src/Form/Meow/Main.php
+++ src/Form/Meow/Main.php
@@ -66,7 +66,6 @@ class Main extends PluginBase
             }
 
         });
-        "{player}" => $player->getName();
         $form->setTitle("SkyBlock Menu");
         $form->setContent("Your SkyBlock Menu!");
         $form->addButton("Your Profile\nClick To View", "0", "textures/ui/icon_steve");
