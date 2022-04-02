453f2422e50d7242dd7cd6da737ebcc4b281083c
Ehan <contact@dadevguy.ml>
2022-04-02 19:02:09 +0500

    Update Main.php

--- src/Form/Meow/Main.php
+++ src/Form/Meow/Main.php
@@ -153,7 +153,7 @@ class Main extends PluginBase
   $form->addButton("§l§eSHOP\n§l§9»» §l§3TAP TO OPEN", 0, $this->getConfig()->get("shopt"));
   $form->addButton("§l§eQUESTS\n§l§9»» §l§3TAP TO OPEN", 0, $this->getConfig()->get("questt"));
   $form->addButton("§l§eENCHANTMENT SHOP\n§l§9»» §l§3TAP TO OPEN", 0, $this->getConfig()->get("enchantshopt"));
-  $form->addButton("§l§eENDER CHEST\n§l§9»» §l§3TAP TO OPEN", 0, $this->()getConfig->get("endechestt"));
+  $form->addButton("§l§eENDER CHEST\n§l§9»» §l§3TAP TO OPEN", 0, $this->getConfig()->get("enderchestt"))
   $form->addButton("§l§eWORK BENCH\n§l§9»» §l§3TAP TO OPEN", 0, $this->()getConfig->get("workbencht"));
   $form->addButton("§l§eAUCTION HOUSE\n§l§9»» §l§3TAP TO OPEN", 0, $this->()getConfig->get("aht"));
   $form->addButton("§l§eBANK\n§l§9»» §l§3TAP TO OPEN", 0, $this->getConfig()->get("bankt"));
